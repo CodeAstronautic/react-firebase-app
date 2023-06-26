9649f7c53c69a35c0af669694148812473c35f27
Pooja mishra <mishrapooja8128@gmail.com>
2023-06-26 16:31:10 +0530

    fixed

---
+++ build/precache-manifest.34947dc4178dc80b87756122a02d4e65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b814ec5739157a66f48f5daf05d9c7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1addd3dc85f37d633de1",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "1addd3dc85f37d633de1",
+    "url": "/static/js/2.823155e2.chunk.js"
+  },
+  {
+    "revision": "b07943acce4b4865451820f522cd70fb",
+    "url": "/static/js/2.823155e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1198e1ac72ee6ee0ded",
+    "url": "/static/js/main.a8d948f4.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.a8d948f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee8c3b90c01e2a4b2e69",
+    "url": "/static/js/runtime-main.007aa73a.js"
+  }
+]);
